1a0a86c070f1005139726c998de649e48bc032e2
Alexisvip100 <alexisultreras01@gmail.com>
2026-07-07 16:55:05 -0600

    feat: redesign Lumina mascot to custom animated scalloped marshmallow character with pencil and apply Notion-style aesthetics

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -5,3 +5,4 @@ export * from './CuteRobotIcon';
 export * from './EmptyState';
 export * from './WorkspaceEmptyState';
 export * from './icons';
+export * from './LuminaAnimatedFace/LuminaAnimatedFace';
